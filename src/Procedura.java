afc89e461060203b0095d2c394a49db3ffec86d4
Mikołaj Czarnecki <mc448206@students.mimuw.edu.pl>
2023-06-14 10:33:55 +0200

    Dodano czesc metod abstrakcyjnych z InstrukcjaBlokowa do WywolanieProcedury. Poprawiono Naglowek. Dodano Liste instrukcji do procedury.

--- src/Procedura.java
+++ src/Procedura.java
@@ -7,10 +7,23 @@ public class Procedura {
     public Procedura(Macchiato program ,String nazwa, List<Character> argumenty) {
         this.program = program;
         this.naglowek = new Naglowek(nazwa, this, argumenty);
+        this.instrukcje = new LinkedList<Instrukcja>();
     }
 
     public Naglowek getNaglowek() {
         return naglowek;
+    }
+
+    public void dodajInstrukcje(Instrukcja instrukcja) {
+        if (instrukcja.getBlokWyzej() == null) {
+            instrukcja.setBlokWyzej(null);
+            instrukcja.setProgram(this.getProgram());
+        }
+        this.instrukcje.add(instrukcja);
+    }
+
+    public List<Instrukcja> getInstrukcje() {
+        return instrukcje;
     }
 
     public Macchiato getProgram() {
--- src/WywolanieProcedury.java
+++ src/WywolanieProcedury.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class WywolanieProcedury extends InstrukcjaBlokowa{
     private List<Deklaracja> deklaracje;
     private Procedura wywolywana;
+
+    private List<Instrukcja> instrukcje;
     public WywolanieProcedury(List<Wyrazenie> argumenty, String nazwa, InstrukcjaBlokowa blokWyzej, Macchiato program) {
         super(blokWyzej, program);
         try{
@@ -22,10 +24,73 @@ public class WywolanieProcedury extends InstrukcjaBlokowa{
                     .add(new Deklaracja(this.wywolywana.getNaglowek().getArgumenty().get(i), argumenty.get(i)));
         }
 
+        this.instrukcje = new LinkedList<Instrukcja>();
+        for (int i = 0; i < this.wywolywana.getInstrukcje().size(); i++) {
+            Instrukcja doDodania = this.wywolywana.getInstrukcje().get(i);
+            doDodania.setBlokWyzej(this);
+
+        }
+
     }
 
     @Override
+    public void wykonajDeklaracje() {
+        int rozmiar = this.deklaracje.size();
+        int j = 0;
+        while (j < rozmiar && !this.getProgram().getCzyBlad()) {
+            try {
+                this.deklaracje.get(j).wykonaj();
+            } catch (RuntimeException e) {
+                if (this.getBlokWyzej() != null) throw new RuntimeException("Runtime exception wyzej");
+                if (!this.getProgram().getCzyBlad()) {
+                    System.out.println(e.getMessage());
+                    this.wypiszWszystkie();
+                    this.getProgram().setCzyBladTrue();
+                }
+            }
+            j++;
+        }
+    }
+    @Override
+    public void setZmienna(char zmienna, int wartosc) {
+        int nrZmiennej = (int) zmienna - (int) 'a';
+        if (this.getZmienneAktywne()[nrZmiennej]) this.getZmienne()[nrZmiennej] = wartosc;
+        else {
+            if (this.getBlokWyzej() == null) throw new RuntimeException("Nie ma zmiennej do przypisania");
+            else this.getBlokWyzej().setZmienna(zmienna, wartosc);
+        }
+    }
+    @Override
+    public int getZmienna(char zmienna) {
+        InstrukcjaBlokowa blokWyzej = this.getBlokWyzej();
+        int nrZmiennej = (int) zmienna - (int) 'a';
+        if (this.getZmienneAktywne()[nrZmiennej]) {
+            return this.getZmienne()[nrZmiennej];
+        } else {
+            if (blokWyzej == null) {
+                throw new RuntimeException("Zmienna nie istnieje");
+            } else {
+                return blokWyzej.getZmienna(zmienna);
+            }
+        }
+    }
+
+    public void wyczyscLokalne() {
+        for (int i = 0; i < 26; i++) {
+            if (this.getZmienneAktywne()[i]) {
+                char zmienna = (char)(i + (int)'a');
+                this.setZmienneAktywne(zmienna,false);
+            }
+        }
+    }
+    @Override
     public String getImie() {
         return("WywolanieProcedury");
+    }
+
+    @Override
+    public void wykonaj() {
+        this.wykonajDeklaracje();
+        int rozmiar =
     }
 }
